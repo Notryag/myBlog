61221c3751ca8742b3391ab4d59823a7191c3eae
Notryag <1135814285@qq.com>
2020-06-07 16:18:37 +0800

    初始化

---
+++ admin/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+  const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home,
+    // children:{
+    //   name:'articles',
+    //   path:'/articles',
+    //   component:()=> import('../views/article.vue')
+    // }
+  },
+  {
+    path: '/content',
+    name: 'Content',
+    component: Home,
+    children:[
+      {name:'articles',
+      path:'articles',
+      component:()=> import('../views/article.vue')}
+    ]
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  }
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
---
+++ admin/src/views/Home.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="home">
+    <el-container style=" height: 100vh;">
+      <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
+        <el-menu :default-openeds="['1']" :default-active="$route.path" router>
+          <el-submenu index="1">
+            <template slot="title"><i class="el-icon-message"></i>内容管理</template>
+            <el-menu-item index="/content/articles">文章</el-menu-item>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title"><i class="el-icon-setting"></i>系统管理</template>
+            <el-menu-item index="1-2">用户管理</el-menu-item>
+            <el-menu-item index="1-3">我的</el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+
+      <el-container>
+        <el-header style="text-align: right; font-size: 12px">
+          <el-dropdown>
+            <i class="el-icon-setting" style="margin-right: 15px"></i>
+            <el-dropdown-menu slot="dropdown">
+              <el-dropdown-item>详情</el-dropdown-item>
+              <el-dropdown-item>登出</el-dropdown-item>
+            </el-dropdown-menu>
+          </el-dropdown>
+          <span>王小虎</span>
+        </el-header>
+        <el-main>
+          <router-view :key="$route.path"></router-view>
+        </el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import LeftMenu from '../components/LeftMenu'
+export default {
+  name: 'Home',
+  data() {
+    const item = {
+      date: '2016-05-02',
+      name: '王小虎',
+      address: '上海市普陀区金沙江路 1518 弄',
+    }
+    return {
+      tableData: Array(10).fill(item),
+    }
+  },
+  components: { LeftMenu },
+}
+</script>
+<style lang="scss">
+.el-header {
+  background-color: #b3c0d1;
+  color: #333;
+  line-height: 60px;
+}
+
+.el-aside {
+  color: #333;
+}
+</style>
